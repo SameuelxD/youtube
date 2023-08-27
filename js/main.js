c730e18fa6bcde61ebb9e0c1e40a45f04ce053c5
Ing.Samuel <126287892+SameuelxD@users.noreply.github.com>
2023-08-27 17:28:46 -0500

    chore :sparkles: styles code and finished html adding javascript for the navigation menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-alert("Iniciando...");
+const container_primary=document.querySelector("#container_primary");
+document.querySelector("#btn_bars").addEventListener('click', () =>{
+    container_primary.classList.toggle("check");
+});
+const checkWidth = () => {
+    if(window.innerWidth <= 768){
+        container_primary.classList.remove("check");
+    }
+    else{
+        container_primary.classList.add("check")
+    }
+}
+checkWidth();
+window.addEventListener('resize',() => {
+    checkWidth();
+});
